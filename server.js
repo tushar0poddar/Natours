58fbd2941857bcaf71c4d72e5b2d84e1781c990d
tushar0poddar <tusharpoddar06@gmail.com>
2023-03-25 17:43:38 +0530

    set up the connection of mongodb atlas on application with mongoose

--- server.js
+++ server.js
@@ -1,8 +1,21 @@
 const dotenv = require('dotenv');
+// eslint-disable-next-line import/no-extraneous-dependencies
+const mongoose = require('mongoose');
 
 dotenv.config({path: './config.env'});
 
 const app = require('./app');
+
+const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
+
+mongoose.connect(DB, {
+    useNewUrlParser:true,
+    useCreateIndex:true,
+    useFindAndModify:false,
+    useUnifiedTopology:true
+}).then(() => {
+    console.log("DB connection successful!!!");
+})
 
 app.listen(process.env.PORT, ()=>{
     console.log("server is running on port 4000");
