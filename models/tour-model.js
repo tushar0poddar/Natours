f9038bb52e53761c28e2d76c9132475bdf5882c4
tushar0poddar <tusharpoddar06@gmail.com>
2023-05-05 17:56:22 +0530

    updating schema

--- models/tour-model.js
+++ models/tour-model.js
@@ -1,72 +1,81 @@
 const mongoose = require('mongoose');
 
-const tourSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'Name of the tour is required'],
-    unique: true,
-    trim: true,
-  },
-  secretTour: {
-    type: Boolean,
-    default: false,
-  },
-  price: {
-    type: Number,
-    required: [true, 'A tour must have a name'],
-  },
-  priceDiscount: {
-    type: Number,
-    validate: {
-      validator: function (val) {
-        return val < this.price;
+const tourSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'Name of the tour is required'],
+      unique: true,
+      trim: true,
+      validate: {
+        validator: function (val) {
+          return typeof val === 'string';
+        },
+        message: 'Name must be a string',
       },
-      message: 'Discount price ({VALUE}) should be less than regular price.',
     },
+    secretTour: {
+      type: Boolean,
+      default: false,
+    },
+    price: {
+      type: Number,
+      required: [true, 'A tour must have a name'],
+    },
+    priceDiscount: {
+      type: Number,
+      validate: {
+        validator: function (val) {
+          return val < this.price;
+        },
+        message: 'Discount price ({VALUE}) should be less than regular price.',
+      },
+    },
+    duration: {
+      type: Number,
+      required: [true, 'A tour must have duration'],
+    },
+    maxGroupSize: {
+      type: Number,
+      required: [true, 'A tour must have a group size.'],
+    },
+    difficulty: {
+      type: String,
+      required: [true, 'A tour must have difficulty'],
+    },
+    ratingAverage: {
+      type: Number,
+      default: 4.3,
+      min: [1, 'The average rating must be 1.0 or above'],
+      max: [5, 'The average rating must be 5.0 or below'],
+    },
+    ratingQuantity: {
+      type: Number,
+      default: 0,
+    },
+    summary: {
+      type: String,
+      trim: true,
+      required: [true, 'A tour must have summary'],
+    },
+    description: {
+      type: String,
+      trim: true,
+    },
+    imageCover: {
+      type: String,
+      required: [true, 'A tour must have cover image'],
+    },
+    images: [String],
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+      select: false,
+    },
+    startDates: [Date],
   },
-  duration: {
-    type: Number,
-    required: [true, 'A tour must have duration'],
-  },
-  maxGroupSize: {
-    type: Number,
-    required: [true, 'A tour must have a group size.'],
-  },
-  difficulty: {
-    type: String,
-    required: [true, 'A tour must have difficulty'],
-  },
-  ratingAverage: {
-    type: Number,
-    default: 4.3,
-    min: [1, 'The average rating must be 1.0 or above'],
-    max: [5, 'The average rating must be 5.0 or below'],
-  },
-  ratingQuantity: {
-    type: Number,
-    default: 0,
-  },
-  summary: {
-    type: String,
-    trim: true,
-    required: [true, 'A tour must have summary'],
-  },
-  description: {
-    type: String,
-    trim: true,
-  },
-  imageCover: {
-    type: String,
-    required: [true, 'A tour must have cover image'],
-  },
-  images: [String],
-  createdAt: {
-    type: Date,
-    default: Date.now(),
-    select: false,
-  },
-  startDates: [Date],
-});
+  { strict: 'throw' }
+);
 
 tourSchema.pre(/^find/, function (next) {
   this.find({ secretTour: { $ne: true } });
